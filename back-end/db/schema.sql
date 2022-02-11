c7354c0e2c8ccecd23d2eb64cba4aa725588b288
Name Ping Louie <namepinglouie@gmail.com>
2022-02-11 16:38:36 -0500

    passed 11 tests total

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -6,9 +6,9 @@ CREATE DATABASE snack_a_log;
 CREATE TABLE snacks (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    fiber NUMERIC,
-    protein NUMERIC,
-    added_sugar NUMERIC,
+    fiber INTEGER DEFAULT 0,
+    protein INTEGER DEFAULT 0,
+    added_sugar INTEGER DEFAULT 0,
     is_healthy BOOLEAN,
     image TEXT
 );
