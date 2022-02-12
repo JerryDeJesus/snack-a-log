d2fa14bfc010b2ce4fca14f8e0cafd4b17f83c07
Name Ping Louie <namepinglouie@gmail.com>
2022-02-12 10:35:08 -0500

    passed 12 tests total

--- back-end/validations/checkValidations.js
+++ back-end/validations/checkValidations.js
@@ -3,17 +3,15 @@ const uppercaseLetters = (req, res, next) => {
     let splitStr = str.split(" ").map(word => {
         if(word.length > 2) {
             word = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-            console.log("modified: ",word);
         }
         else if(word.length < 3) {
             word = word.toLowerCase();
         }
         return word;
     })
-    console.log("Testing: ",splitStr.join(" "));
     req.body.name = splitStr.join(" ");
     next();
-}
+};
 
 module.exports = {
     uppercaseLetters
